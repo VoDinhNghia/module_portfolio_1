9a14c541bde26cacd02bda770fab43a3f4b07295
Võ Đình Nghĩa <56909768+VoDinhNghia@users.noreply.github.com>
2020-06-01 19:58:29 +0700

    Update module_portfolio_new_1.py

--- New_module/module_portfolio_new_1.py
+++ New_module/module_portfolio_new_1.py
@@ -733,4 +733,27 @@ class portfolio:
             
         index_std_mini = stdev_frontier.index(min(stdev_frontier))
         return stdev_frontier[0:index_std_mini], mean_frontier[0:index_std_mini]
-    
+    def point_M(self):
+        r_free = 18
+        arr_mean = self.compution_mean()
+        arr_stdev = self.compution_stdev() 
+        mean_frontier = self.frontier_eff()[1]
+        arr_ratio = []
+        for m in mean_frontier:
+            ratio = (m - r_free)/arr_stdev[arr_mean.index(m)]
+            arr_ratio.append(ratio)
+        max_ratio = max(arr_ratio)
+        index_M = arr_ratio.index(max_ratio)
+        value_M_mean = arr_mean[index_M]
+        value_M_std = arr_stdev[index_M]
+        return value_M_std, value_M_mean
+
+    def CML(self):
+        M_std, M_mean = self.point_M()
+        r_free = 18  
+        arr_mean_cml = []
+        std_exp = np.arange(0, 8, 0.5)
+        for x in std_exp:
+            mean_exp = r_free + (x*(M_mean-r_free))/M_std
+            arr_mean_cml.append(mean_exp)
+        return std_exp, arr_mean_cml
