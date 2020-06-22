2bd21d1b50800ffc5b4224a22a9b59d0d233b04b
Võ Đình Nghĩa <56909768+VoDinhNghia@users.noreply.github.com>
2020-06-22 16:08:34 +0700

    Update test_1.py

--- module1/test_1.py
+++ module1/test_1.py
@@ -3,27 +3,12 @@ import matplotlib.pyplot as plt
 import module_portfolio_new_1
 from module_portfolio_new_1 import portfolio, portfolio_n_assets, computation_mean
 
-a = [[1,6,2],[4,7,16],[7,2,9]]
+a = [[10, 30, 68, 80],[56, 64, 73, 67],[100, 105, 115, 150]]
 n, mean_exp, std_exp, corr, cov = computation_mean(a).compu_mean()
 
-print(corr)
+print(computation_mean(a).comp_list_price())
 
 ar_std, ar_mean, w = computation_mean(a).compu_port()
 std_mini, mean_mini = computation_mean(a).minimum_std()
 M = computation_mean(a).market_port()
 std_cml, mean_cml = computation_mean(a).CML_port()
-
-plt.figure(figsize=(10, 5))
-plt.scatter(ar_std, ar_mean, c=ar_mean / ar_std, marker='o')
-plt.plot(std_mini, mean_mini, 'r*',markersize=20, label='minimum')
-plt.plot(M[0],M[1],'g*',markersize=20, label='Market portfolio')
-plt.plot(std_cml, mean_cml, 'y-', markersize=2, label='CML')
-plt.plot(std_exp[0],mean_exp[0],'g*', markersize=15, label='asset 1')
-plt.plot(std_exp[1],mean_exp[1],'b*', markersize=15, label='asset 2')
-plt.plot(std_exp[2],mean_exp[2],'r*', markersize=15, label='asset 3')
-plt.grid(True)
-plt.xlabel('expected volatility')
-plt.ylabel('expected return')
-plt.colorbar(label='ratio mean/stdev') 
-plt.legend(loc='best')#phải plt.legend có label mới hiển thị
-plt.show()
